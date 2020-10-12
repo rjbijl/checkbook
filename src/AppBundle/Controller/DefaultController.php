5a59ccedc901ee649060077acd0a56f32b5d6df7
Robert-Jan Bijl <rjbijl@gmail.com>
2020-10-12 21:35:38 +0200

    monthly overview

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,14 +9,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
-     * @Template
+     *
      * @param Request $request
-     * @return array
+     * @return Response
      */
     public function indexAction(Request $request)
     {
@@ -38,10 +39,63 @@ class DefaultController extends Controller
         }
 
         // replace this example code with whatever you need
-        return [
+        return $this->render('@App/Default/index.html.twig', [
             'filterForm' => $filterForm->createView(),
             'mutations' => $mutations,
             'total' => $total,
-        ];
+        ]);
+    }
+
+    /**
+     * @Route("/monthly", name="monthly")
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function monthlyAction(Request $request)
+    {
+        $ownAccounts = ['NL78RABO1472296273', 'NL37INGB0008212053', 'NL48RABO0145123219', 'NL52NNBA2027725776'];
+
+        $months = [];
+        /** @var Mutation $mutation */
+        foreach($this->getDoctrine()->getRepository(Mutation::class)->findAll() as $mutation) {
+            if (in_array($mutation->getContraAccountNumber(), $ownAccounts)) {
+                continue;
+            }
+
+            if (!isset($months[$mutation->getDate()->format('Ym')])) {
+                $months[$mutation->getDate()->format('Ym')] = [
+                    'categories' => [],
+                    'amount' => 0,
+                ];
+            }
+
+            $category = $mutation->getCategory() ? $mutation->getCategory()->getName() : 'Overig';
+            if (!isset($months[$mutation->getDate()->format('Ym')]['categories'][$category])) {
+                $months[$mutation->getDate()->format('Ym')]['categories'][$category] = [
+                    'amount' => 0,
+                    'mutations' => [],
+                ];
+            }
+
+            $months[$mutation->getDate()->format('Ym')]['categories'][$category]['mutations'][] = $mutation;
+
+            switch ($mutation->getType()) {
+                case Mutation::TYPE_CREDIT:
+                    $months[$mutation->getDate()->format('Ym')]['amount'] -= $mutation->getAmount();
+                    $months[$mutation->getDate()->format('Ym')]['categories'][$category]['amount'] -= $mutation->getAmount();
+                    break;
+                case Mutation::TYPE_DEBIT:
+                    $months[$mutation->getDate()->format('Ym')]['amount'] += $mutation->getAmount();
+                    $months[$mutation->getDate()->format('Ym')]['categories'][$category]['amount'] += $mutation->getAmount();
+                    break;
+            }
+        }
+
+        krsort($months);
+
+        return $this->render('@App/Default/monthly.html.twig', [
+            'months' => $months,
+        ]);
     }
 }
--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -168,4 +168,9 @@ class Category
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
